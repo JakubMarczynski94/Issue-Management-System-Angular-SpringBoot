3c4f52bc8fb64e38d98441fb51b4cfaec15f485b
philljohnson <philljohnson739@gmail.com>
2019-04-23 22:18:03 +0300

    adding form and first post operation for project

--- webui/src/app/pages/project/project.component.ts
+++ webui/src/app/pages/project/project.component.ts
@@ -3,15 +3,18 @@ import { ProjectService } from 'src/app/services/shared/project.service';
 import { Page } from 'src/app/common/Page';
 import { Project } from 'src/app/common/project.module';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap';
+import { Validators, FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
-  styleUrls: ['./project.component.css']
+  styleUrls: ['./project.component.css'],
+
 })
 export class ProjectComponent implements OnInit {
   page = new Page();
   rows = new Array<Project>();
+  projectForm: FormGroup;
   modalRef: BsModalRef;
   projectTitle: string;
   cols = [
@@ -19,18 +22,45 @@ export class ProjectComponent implements OnInit {
     { prop: 'projectName', name: 'Project Name', sortable: false },
     { prop: 'projectCode', name: 'Project Code', sortable: false },
   ];
-  constructor(private projectService: ProjectService, private modalService: BsModalService) {
+  constructor(private projectService: ProjectService,
+              private modalService: BsModalService,
+              private formBuilder: FormBuilder) {
     this.projectTitle = 'Project Details';
   }
 
   ngOnInit() {
     this.setPage({ offset: 0 });
     this.projectTitle = 'Project Details Extra';
+    this.projectForm = this.formBuilder.group({
+      'projectCode': [null, [Validators.required, Validators.minLength(2), Validators.maxLength(10)]],
+      'projectName': [null, [Validators.required, Validators.minLength(4)]]
+    });
+  }
+
+  get f(){
+    return this.projectForm.controls;
+  }
+  saveProject(){
+    if(!this.projectForm.valid){
+      return ;
+    }
+    this.projectService.createProject(this.projectForm.value).subscribe(
+      res=>{
+        console.log(res);
+      }
+    );
+    this.closeAndResetModal();
+    this.setPage({ offset: 0 });
+  }
+  closeAndResetModal(){
+    this.projectForm.reset();
+    this.modalRef.hide();
   }
 
   openModal(template: TemplateRef<any>) {
     this.modalRef = this.modalService.show(template);
   }
+
   setPage(pageInfo) {
     this.page.page = pageInfo.offset;
     this.projectService.getAllPageable(this.page).subscribe(pagedData => {
--- webui/src/app/shared/shared.module.ts
+++ webui/src/app/shared/shared.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 import {BsModalRef, ModalModule} from 'ngx-bootstrap';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ModalModule.forRoot()
   ],
   providers: [BsModalRef],
   exports: [
     TranslateModule,
-    ModalModule
-]
+    ModalModule,
+    ReactiveFormsModule,
+  ]
 })
 export class SharedModule { }
