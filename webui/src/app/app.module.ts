fa4556a7e7cd46ed8c10cc0f60c4b37e79f84f6b
webmaster1022 <seniordev1019@gmail.com>
2019-05-27 14:15:37 +0300

    loggin and insert security and jwt for front and backend

--- webui/src/app/app.module.ts
+++ webui/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing.module';
 import { FooterComponent, HeaderComponent, SidebarComponent, AppLayoutComponent } from './_layout';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { ApiService } from './services/api.service';
@@ -13,6 +13,11 @@ import { IssueService } from './services/shared/issue.service';
 import { UserService } from './services/shared/user.service';
 import { IssueHistoryService } from './services/shared/issue.history.service';
 import { NotfoundComponent } from './shared/notfound/notfound.component';
+import { JwtInterceptor } from './security/jwt.interceptor';
+import { ErrorInterceptor } from './security/authentication.interceptor';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 export const createTranslateLoader = (http: HttpClient) => {
   return new TranslateHttpLoader(http, './assets/lang/', '.json');
@@ -24,12 +29,16 @@ export const createTranslateLoader = (http: HttpClient) => {
     FooterComponent,
     HeaderComponent,
     SidebarComponent,
-    NotfoundComponent 
+    NotfoundComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
     NgxDatatableModule,
     AppRoutingModule,
     TranslateModule.forRoot({
@@ -40,7 +49,14 @@ export const createTranslateLoader = (http: HttpClient) => {
       }
     })
   ],
-  providers: [ApiService, ProjectService, IssueService, UserService, IssueHistoryService],
+  providers: [
+    ApiService,
+    ProjectService,
+    IssueService, UserService,
+    IssueHistoryService,
+    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
